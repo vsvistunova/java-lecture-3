4beac069b51ce20a5fb2d2f35b29024fbe409630
Свистунова Виктория Романовна (svistunova_vr) <svistunova_vr@nlmk.com>
2025-10-08 17:47:34 +0300

    Initial commit

---
+++ src/main/java/com/university/java_lecture_3/JavaLecture3Application.java
@@ -0,0 +1,13 @@
+package com.university.java_lecture_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLecture3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLecture3Application.class, args);
+	}
+
+}
