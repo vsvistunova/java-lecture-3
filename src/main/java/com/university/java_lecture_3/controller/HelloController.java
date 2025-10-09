f8fb1496c8c3e4fe4cd557b6bea556b7ba088300
Свистунова Виктория Романовна (svistunova_vr) <svistunova_vr@nlmk.com>
2025-10-09 10:32:11 +0300

    Added Hello endpoint

---
+++ src/main/java/com/university/java_lecture_3/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.university.java_lecture_3.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  @GetMapping("/hello")
+  public String sayHello() {
+    return "Привет!";
+  }
+}
+
