ad09bc5471cb59bfcd29df1a7715fcfc45dce278
PiVo-vrh <vitikoaz.harlen09@gmail.com>
2025-10-16 16:00:49 +0300

    Добавил GET функцию /time

--- src/main/java/com/university/java_lecture_3/controller/HelloController.java
+++ src/main/java/com/university/java_lecture_3/controller/HelloController.java
@@ -3,6 +3,9 @@ package com.university.java_lecture_3.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 @RestController
 public class HelloController {
 
@@ -10,5 +13,10 @@ public class HelloController {
   public String sayHello() {
     return "Привет!";
   }
+  @GetMapping("/time")
+    public static ZonedDateTime sayTime() {
+        ZonedDateTime date = ZonedDateTime.now(ZoneId.of("Europe/Moscow"));
+        System.out.println(date);  // Выводит, например: 2025-10-11T22:09:06.000+03:00[Europe/Moscow]
+        return date;
+    }
 }
-
