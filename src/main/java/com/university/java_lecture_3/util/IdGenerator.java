3b072bc7d25af1079b0188e3820224e13b939691
ferty460 <rostovcevvladimir460@gmail.com>
2025-10-17 20:29:05 +0300

    feat: add automatic ID generation for entities

---
+++ src/main/java/com/university/java_lecture_3/util/IdGenerator.java
@@ -0,0 +1,28 @@
+package com.university.java_lecture_3.util;
+
+import lombok.experimental.UtilityClass;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@UtilityClass
+public class IdGenerator {
+
+    private static final int INITIAL_ID = 1;
+    private static final Map<Class<?>, AtomicLong> ENTITY_ID_COUNTER;
+
+    static {
+        ENTITY_ID_COUNTER = new ConcurrentHashMap<>();
+    }
+
+    public static long generate(Class<?> clazz) {
+        if (ENTITY_ID_COUNTER.containsKey(clazz)) {
+            return ENTITY_ID_COUNTER.get(clazz).incrementAndGet();
+        } else {
+            ENTITY_ID_COUNTER.put(clazz, new AtomicLong(INITIAL_ID));
+            return ENTITY_ID_COUNTER.get(clazz).get();
+        }
+    }
+
+}
