d94702cf1fe9c09cc1ec47032bc9b45eb817b518
VS-ultra <sevachev.vlad@mail.ru>
2025-10-19 21:08:37 +0300

    seconds steps

---
+++ java-lecture-3/src/main/java/com/example/java_lecture_3/model/Event.java
@@ -0,0 +1,32 @@
+package com.example.java_lecture_3.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+    private Long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    private String description;
+
+    @NotNull(message = "DateTime is mandatory")
+    private LocalDateTime dateTime;
+
+    @NotBlank(message = "Location is mandatory")
+    private String location;
+
+    private Integer capacity;
+
+    private List<User> attendees;
+}
